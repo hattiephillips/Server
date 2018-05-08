b0164f9be06e91cbf9eb3f7a96d72225a9337660
hattiephillips <harriet-phillips@hotmail.co.uk>
2018-05-08 11:54:25 -0400

    supporting file for data upload form Question App and server.js

---
+++ uploadData.js
@@ -0,0 +1,98 @@
+<script>
+//Code adapted from practical sessions of CEGEG077 2018.
+function startDataUpload() {
+	alert (“start data upload”);
+}
+</script>
+<script>
+// radio button values Question 1
+if (document.getElementById("1777").checked) {
+        postString=postString+"&question1=1777";
+}
+if (document.getElementById("1888").checked) {
+postString=postString+"&question1=1888";
+}
+if (document.getElementById("1999").checked) {
+        postString=postString+"&question1=1999";
+}
+if (document.getElementById("2018").checked) {
+postString=postString+"&question1=2018";
+}
+</script>
+<script>
+// radio button values Question 2
+if (document.getElementById("James Chadwick").checked) {
+        postString=postString+"&question2=James Chadwick";
+}
+if (document.getElementById("Chadwick Boseman").checked) {
+postString=postString+"&question2=Chadwick Boseman";
+}
+if (document.getElementById("Edwin Chadwick").checked) {
+        postString=postString+"&question2=Edwin Chadwick";
+}
+if (document.getElementById("Helen Chadwick").checked) {
+postString=postString+"&question2=Helen Chadwick";
+}
+</script>
+<script>
+// radio button values Question 3
+if (document.getElementById("Gladiator").checked) {
+        postString=postString+"&question3=Gladiator";
+}
+if (document.getElementById("Lego Movie").checked) {
+postString=postString+"&question3=Lego Movie";
+}
+if (document.getElementById("Horrible History").checked) {
+        postString=postString+"&question3=Horrible History";
+}
+if (document.getElementById("Pride and Prejudice").checked) {
+postString=postString+"&question3=Pride and Prejudice";
+}
+</script>
+<script>
+// radio button values Question 4
+if (document.getElementById("2").checked) {
+        postString=postString+"&question4=2";
+}
+if (document.getElementById("29").checked) {
+postString=postString+"&question4=29";
+}
+if (document.getElementById("50").checked) {
+        postString=postString+"&question4=50";
+}
+if (document.getElementById("100").checked) {
+postString=postString+"&question4=100";
+}
+</script>
+<script>
+// radio button values Question 5
+if (document.getElementById("Harry Potter").checked) {
+        postString=postString+"&question5=Harry Potter";
+}
+if (document.getElementById("Lego Batman Movie").checked) {
+postString=postString+"&question5=Lego Batman Movie";
+}
+if (document.getElementById("Batman Begins").checked) {
+        postString=postString+"&question5=Batman Begins";
+}
+if (document.getElementById("Star Wars").checked) {
+postString=postString+"&question5=Star Wars";
+}
+</script>
+<script>
+// radio button values Question 6
+if (document.getElementById("2018").checked) {
+        postString=postString+"&question6=2018";
+}
+if (document.getElementById("1990").checked) {
+postString=postString+"&question6=1990";
+}
+if (document.getElementById("1880").checked) {
+        postString=postString+"&question6=1880";
+}
+if (document.getElementById("1878").checked) {
+postString=postString+"&question6=1878";
+}
+</script>
+
+
